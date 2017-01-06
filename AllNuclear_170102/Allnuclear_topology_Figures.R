54684a9f6b1b0d4f7ca657918179f89d629c5aaf
Evan Forsythe <esforsythe@email.arizona.edu>
2017-01-05 19:53:05 -0700

    Added block jackkifing!

--- AllNuclear_170102/Allnuclear_topology_Figures.R
+++ AllNuclear_170102/Allnuclear_topology_Figures.R
@@ -6,6 +6,8 @@ setwd("/Users/esforsythe/Documents/Beilstiein_lab_research/BIOINFORMATICS/Brassi
 #Install and load packages
 install.packages("boot")
 library(boot)
+install.packages("resample")
+library(resample)
 
 #read in dataframe
 data<-read.csv("Results_table_170103.csv", header=TRUE)
@@ -38,7 +40,6 @@ Boot_Dstat <- function(X=data_noOthers$Topology_loose){
   Dstat_temp<-unname(Dstat_temp)
   Dstat_temp
 }
-N<- 10000
 #Perform bootstrap replication
 N<- 10000 #This indicates how many replicates to use
 boot.replicate <- replicate(N, Boot_Dstat())
@@ -46,7 +47,7 @@ boot.replicate <- replicate(N, Boot_Dstat())
 SD<-sd(boot.replicate)
 z<-(Dstat_observed-0)/SD
 P_value<-2*pnorm(-abs(z))
-out_full<-c(AC_count, AB_count, Dstat_observed, SD, P_value)
+out_full<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
 #############################################################
 ###Now do the Dstat calculation for individual chromosomes###
@@ -81,7 +82,7 @@ boot.replicate <- replicate(N, Boot_Dstat())
 SD<-sd(boot.replicate)
 z<-(Dstat_observed-0)/SD
 P_value<-2*pnorm(-abs(z))
-out_chrom1<-c(AC_count, AB_count, Dstat_observed, SD, P_value)
+out_chrom1<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
 #CHROM 2#
 chrom2_data<-subset(data_noOthers, Scaffold == 2)
@@ -112,7 +113,7 @@ boot.replicate <- replicate(N, Boot_Dstat())
 SD<-sd(boot.replicate)
 z<-(Dstat_observed-0)/SD
 P_value<-2*pnorm(-abs(z))
-out_chrom2<-c(AC_count, AB_count, Dstat_observed, SD, P_value)
+out_chrom2<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
 #CHROM 3#
 chrom3_data<-subset(data_noOthers, Scaffold == 3)
@@ -143,7 +144,7 @@ boot.replicate <- replicate(N, Boot_Dstat())
 SD<-sd(boot.replicate)
 z<-(Dstat_observed-0)/SD
 P_value<-2*pnorm(-abs(z))
-out_chrom3<-c(AC_count, AB_count, Dstat_observed, SD, P_value)
+out_chrom3<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
 #CHROM 4#
 chrom4_data<-subset(data_noOthers, Scaffold == 4)
@@ -174,7 +175,7 @@ boot.replicate <- replicate(N, Boot_Dstat())
 SD<-sd(boot.replicate)
 z<-(Dstat_observed-0)/SD
 P_value<-2*pnorm(-abs(z))
-out_chrom4<-c(AC_count, AB_count, Dstat_observed, SD, P_value)
+out_chrom4<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
 #CHROM 5#
 chrom5_data<-subset(data_noOthers, Scaffold == 5)
@@ -205,7 +206,7 @@ boot.replicate <- replicate(N, Boot_Dstat())
 SD<-sd(boot.replicate)
 z<-(Dstat_observed-0)/SD
 P_value<-2*pnorm(-abs(z))
-out_chrom5<-c(AC_count, AB_count, Dstat_observed, SD, P_value)
+out_chrom5<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
 #CHROM 6#
 chrom6_data<-subset(data_noOthers, Scaffold == 6)
@@ -236,7 +237,7 @@ boot.replicate <- replicate(N, Boot_Dstat())
 SD<-sd(boot.replicate)
 z<-(Dstat_observed-0)/SD
 P_value<-2*pnorm(-abs(z))
-out_chrom6<-c(AC_count, AB_count, Dstat_observed, SD, P_value)
+out_chrom6<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
 #CHROM 7#
 chrom7_data<-subset(data_noOthers, Scaffold == 7)
@@ -267,7 +268,7 @@ boot.replicate <- replicate(N, Boot_Dstat())
 SD<-sd(boot.replicate)
 z<-(Dstat_observed-0)/SD
 P_value<-2*pnorm(-abs(z))
-out_chrom7<-c(AC_count, AB_count, Dstat_observed, SD, P_value)
+out_chrom7<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
 #CHROM 8#
 chrom8_data<-subset(data_noOthers, Scaffold == 8)
@@ -298,40 +299,96 @@ boot.replicate <- replicate(N, Boot_Dstat())
 SD<-sd(boot.replicate)
 z<-(Dstat_observed-0)/SD
 P_value<-2*pnorm(-abs(z))
-out_chrom8<-c(AC_count, AB_count, Dstat_observed, SD, P_value)
+out_chrom8<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
+#Concat the results from all the chromosomes
 cat_out<-c(out_full, out_chrom1, out_chrom2, out_chrom3, out_chrom4, out_chrom5, out_chrom6, out_chrom7, out_chrom8)
 
-
+#convert into dataframe
 cat_out_df<- data.frame(matrix(unlist(cat_out), nrow=9, byrow=TRUE))
-names(cat_out_df) <- c("AC_trees", "AB_trees", "Dstat_obs", "SD", "P_value")
+
+#append column names and rownames
+names(cat_out_df) <- c("AC_trees", "AB_trees", "Dstat_obs", "SD", "Z_score", "P_value")
+rownames(cat_out_df) <- c("Full_genome", "Chrom1", "Chrom2", "Chrom3", "Chrom4", "Chrom5", "Chrom6", "Chrom7", "Chrom8")
 
 cat_out_df
 
 
+#####BLOCK JACKKNIFING######
+#Before performing this, you need to make sure 
+#the topology data file is sorted by chromosomal 
+#position
 
-
-
-
-
-
-
-
-
-
-####filter for only well supported topologies (>=70)
-
-data_noOthers70BS<-subset(data_noOthers, Bootstrap_Support >= 70)
-
-table<-table(data_noOthers70BS$Topology_loose)
-
+table<-table(data_noOthers$Topology_loose)
 AB_count<-table["AB_topology"]
 AC_count<-table["AC_topology"]
 BC_count<-table["BC_topology"]
 
+#Calculate the Dstat
 Dstat_observed<-(AC_count - AB_count)/(AC_count + AB_count)
 Dstat_observed<-unname(Dstat_observed)
 
+n<-nrow(data_noOthers)
+block_size<-round((n/100)-1)
+index<-i-1
+jackout<-list()
+
+for(i in 1:100) {
+ #remove the first block
+  if (i == 1){sample_data<-data_noOthers[-c((1):(1+block_size)),]
+  }else {
+  #remove all the other blocks
+    sample_data<-data_noOthers[-c((i*block_size):((i*block_size)+block_size)),]
+  }
+  #Find the topologies for the rep
+  table<-table(sample_data$Topology_loose)
+  AB_count<-table["AB_topology"]
+  AC_count<-table["AC_topology"]
+  BC_count<-table["BC_topology"]
+  
+  #Calculate the Dstat
+  Dstat_rep<-(AC_count - AB_count)/(AC_count + AB_count)
+  Dstat_rep<-unname(Dstat_rep)
+  #print(Dstat_rep)
+  jackout[[i]]<-Dstat_rep
+  
+}
+#clean up the list
+jackout<-unlist(jackout)
+#Calculate the P-value
+SD<-sd(jackout)
+z<-(Dstat_observed-0)/SD
+P_value<-2*pnorm(-abs(z))
+
+##############################################################################
+##############################################################################
+##############################################################################
+###########filter for only well supported topologies (>=70)###################
+##############################################################################
+##############################################################################
+##############################################################################
+
+data_noOthers70BS<-subset(data_noOthers, Bootstrap_Support >= 70)
+
+#make pie chart (BS scores >= 70)
+labels_loose<-names(summary(data_noOthers70BS$Topology_loose))
+labels_loose<-paste(labels_loose, summary(data_noOthers70BS$Topology_loose))
+pie(summary(data_noOthers70BS$Topology_loose), labels=labels_loose, main="Nuclear Topologies (loose)\nBS>=70%")
+
+
+###########D-STAT#############
+
+#Find counts of each topology
+table<-table(data_noOthers70BS$Topology_loose)
+AB_count<-table["AB_topology"]
+AC_count<-table["AC_topology"]
+BC_count<-table["BC_topology"]
+
+#Calculate the Dstat
+Dstat_observed<-(AC_count - AB_count)/(AC_count + AB_count)
+Dstat_observed<-unname(Dstat_observed)
+
+#Create function for bootstrapping
 Boot_Dstat <- function(X=data_noOthers70BS$Topology_loose){
   x.boot<-sample(X, size=length(X), replace=TRUE)
   table<-table(x.boot)
@@ -341,63 +398,294 @@ Boot_Dstat <- function(X=data_noOthers70BS$Topology_loose){
   Dstat_temp<-unname(Dstat_temp)
   Dstat_temp
 }
-
-N<- 10000
 #Perform bootstrap replication
+N<- 10000 #This indicates how many replicates to use
 boot.replicate <- replicate(N, Boot_Dstat())
 #Calculate P-value
 SD<-sd(boot.replicate)
 z<-(Dstat_observed-0)/SD
 P_value<-2*pnorm(-abs(z))
+out_full<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
-#make pie chart (BS scores >= 70)
-labels_loose<-names(summary(data_noOthers70BS$Topology_loose))
-labels_loose<-paste(labels_loose, summary(data_noOthers70BS$Topology_loose))
-pie(summary(data_noOthers70BS$Topology_loose), labels=labels_loose, main="Nuclear Topologies (loose)\nBS>=70%")
+#############################################################
+###Now do the Dstat calculation for individual chromosomes###
+#############################################################
 
+#CHROM 1#
+chrom1_data<-subset(data_noOthers70BS, Scaffold == 1)
 
+table<-table(chrom1_data$Topology_loose)
+AB_count<-table["AB_topology"]
+AC_count<-table["AC_topology"]
+BC_count<-table["BC_topology"]
 
+#Calculate the Dstat
+Dstat_observed<-(AC_count - AB_count)/(AC_count + AB_count)
+Dstat_observed<-unname(Dstat_observed)
 
+#Create function for bootstrapping
+Boot_Dstat <- function(X=chrom1_data$Topology_loose){
+  x.boot<-sample(X, size=length(X), replace=TRUE)
+  table<-table(x.boot)
+  AB_count<-table["AB_topology"]
+  AC_count<-table["AC_topology"]
+  Dstat_temp<-(AC_count - AB_count)/(AC_count + AB_count)
+  Dstat_temp<-unname(Dstat_temp)
+  Dstat_temp
+}
+#Perform bootstrap replication
+N<- 10000 #This indicates how many replicates to use
+boot.replicate <- replicate(N, Boot_Dstat())
+#Calculate P-value
+SD<-sd(boot.replicate)
+z<-(Dstat_observed-0)/SD
+P_value<-2*pnorm(-abs(z))
+out_chrom1<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
+#CHROM 2#
+chrom2_data<-subset(data_noOthers70BS, Scaffold == 2)
 
+table<-table(chrom2_data$Topology_loose)
+AB_count<-table["AB_topology"]
+AC_count<-table["AC_topology"]
+BC_count<-table["BC_topology"]
 
+#Calculate the Dstat
+Dstat_observed<-(AC_count - AB_count)/(AC_count + AB_count)
+Dstat_observed<-unname(Dstat_observed)
 
+#Create function for bootstrapping
+Boot_Dstat <- function(X=chrom2_data$Topology_loose){
+  x.boot<-sample(X, size=length(X), replace=TRUE)
+  table<-table(x.boot)
+  AB_count<-table["AB_topology"]
+  AC_count<-table["AC_topology"]
+  Dstat_temp<-(AC_count - AB_count)/(AC_count + AB_count)
+  Dstat_temp<-unname(Dstat_temp)
+  Dstat_temp
+}
+#Perform bootstrap replication
+N<- 10000 #This indicates how many replicates to use
+boot.replicate <- replicate(N, Boot_Dstat())
+#Calculate P-value
+SD<-sd(boot.replicate)
+z<-(Dstat_observed-0)/SD
+P_value<-2*pnorm(-abs(z))
+out_chrom2<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
+#CHROM 3#
+chrom3_data<-subset(data_noOthers70BS, Scaffold == 3)
 
-############failed attempts at bootstrapping############
+table<-table(chrom3_data$Topology_loose)
+AB_count<-table["AB_topology"]
+AC_count<-table["AC_topology"]
+BC_count<-table["BC_topology"]
 
-##Here I tried to replicate the evobiR calcD
-#boot.replicate[is.nan(boot.replicate)] <- 0
-#StanD<-sd(boot.replicate)
-#z<-abs(Dstat_observed/StanD)
-#pval<- 2 * (1- pnorm(z))
-#pval<-unname(pval)
-#pval
+#Calculate the Dstat
+Dstat_observed<-(AC_count - AB_count)/(AC_count + AB_count)
+Dstat_observed<-unname(Dstat_observed)
 
-###Create D stat function (try1)
-#Dstat_func<-function (AB_count, AC_count) {
-#  table<-table(data_noOthers$Topology_loose)
-#  AB_count<-table["AB_topology"]
-#  AC_count<-table["AC_topology"]
-#  #stat_temp<-(AC_count - AB_count)/(AC_count + AB_count)
-#  #Dstat_temp<-unname(Dstat_temp)
-#  return(AB_count, AC_count)
-#}
+#Create function for bootstrapping
+Boot_Dstat <- function(X=chrom3_data$Topology_loose){
+  x.boot<-sample(X, size=length(X), replace=TRUE)
+  table<-table(x.boot)
+  AB_count<-table["AB_topology"]
+  AC_count<-table["AC_topology"]
+  Dstat_temp<-(AC_count - AB_count)/(AC_count + AB_count)
+  Dstat_temp<-unname(Dstat_temp)
+  Dstat_temp
+}
+#Perform bootstrap replication
+N<- 10000 #This indicates how many replicates to use
+boot.replicate <- replicate(N, Boot_Dstat())
+#Calculate P-value
+SD<-sd(boot.replicate)
+z<-(Dstat_observed-0)/SD
+P_value<-2*pnorm(-abs(z))
+out_chrom3<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
-###Create D stat function (try2)
-#Dstat_func<-function (tops) {
-#  table<-table(tops)
-#  AB_count<-table["AB_topology"]
-#  AC_count<-table["AC_topology"]
-#  Dstat_temp<-(AC_count - AB_count)/(AC_count + AB_count)
-#  Dstat_temp<-unname(Dstat_temp)
-#  return(Dstat_temp)
-#}
+#CHROM 4#
+chrom4_data<-subset(data_noOthers70BS, Scaffold == 4)
 
-###perform bootstrap resample
-#boot(data=data_noOthers$Topology_loose, statistic=Dstat_func, R=1000, sim="parametric", stype="i",
-#     formula=(AC_count - AB_count)/(AC_count + AB_count))
+table<-table(chrom4_data$Topology_loose)
+AB_count<-table["AB_topology"]
+AC_count<-table["AC_topology"]
+BC_count<-table["BC_topology"]
 
+#Calculate the Dstat
+Dstat_observed<-(AC_count - AB_count)/(AC_count + AB_count)
+Dstat_observed<-unname(Dstat_observed)
 
+#Create function for bootstrapping
+Boot_Dstat <- function(X=chrom4_data$Topology_loose){
+  x.boot<-sample(X, size=length(X), replace=TRUE)
+  table<-table(x.boot)
+  AB_count<-table["AB_topology"]
+  AC_count<-table["AC_topology"]
+  Dstat_temp<-(AC_count - AB_count)/(AC_count + AB_count)
+  Dstat_temp<-unname(Dstat_temp)
+  Dstat_temp
+}
+#Perform bootstrap replication
+N<- 10000 #This indicates how many replicates to use
+boot.replicate <- replicate(N, Boot_Dstat())
+#Calculate P-value
+SD<-sd(boot.replicate)
+z<-(Dstat_observed-0)/SD
+P_value<-2*pnorm(-abs(z))
+out_chrom4<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
 
+#CHROM 5#
+chrom5_data<-subset(data_noOthers70BS, Scaffold == 5)
+
+table<-table(chrom1_data$Topology_loose)
+AB_count<-table["AB_topology"]
+AC_count<-table["AC_topology"]
+BC_count<-table["BC_topology"]
+
+#Calculate the Dstat
+Dstat_observed<-(AC_count - AB_count)/(AC_count + AB_count)
+Dstat_observed<-unname(Dstat_observed)
+
+#Create function for bootstrapping
+Boot_Dstat <- function(X=chrom5_data$Topology_loose){
+  x.boot<-sample(X, size=length(X), replace=TRUE)
+  table<-table(x.boot)
+  AB_count<-table["AB_topology"]
+  AC_count<-table["AC_topology"]
+  Dstat_temp<-(AC_count - AB_count)/(AC_count + AB_count)
+  Dstat_temp<-unname(Dstat_temp)
+  Dstat_temp
+}
+#Perform bootstrap replication
+N<- 10000 #This indicates how many replicates to use
+boot.replicate <- replicate(N, Boot_Dstat())
+#Calculate P-value
+SD<-sd(boot.replicate)
+z<-(Dstat_observed-0)/SD
+P_value<-2*pnorm(-abs(z))
+out_chrom5<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
+
+#CHROM 6#
+chrom6_data<-subset(data_noOthers70BS, Scaffold == 6)
+
+table<-table(chrom6_data$Topology_loose)
+AB_count<-table["AB_topology"]
+AC_count<-table["AC_topology"]
+BC_count<-table["BC_topology"]
+
+#Calculate the Dstat
+Dstat_observed<-(AC_count - AB_count)/(AC_count + AB_count)
+Dstat_observed<-unname(Dstat_observed)
+
+#Create function for bootstrapping
+Boot_Dstat <- function(X=chrom6_data$Topology_loose){
+  x.boot<-sample(X, size=length(X), replace=TRUE)
+  table<-table(x.boot)
+  AB_count<-table["AB_topology"]
+  AC_count<-table["AC_topology"]
+  Dstat_temp<-(AC_count - AB_count)/(AC_count + AB_count)
+  Dstat_temp<-unname(Dstat_temp)
+  Dstat_temp
+}
+#Perform bootstrap replication
+N<- 10000 #This indicates how many replicates to use
+boot.replicate <- replicate(N, Boot_Dstat())
+#Calculate P-value
+SD<-sd(boot.replicate)
+z<-(Dstat_observed-0)/SD
+P_value<-2*pnorm(-abs(z))
+out_chrom6<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
+
+#CHROM 7#
+chrom7_data<-subset(data_noOthers70BS, Scaffold == 7)
+
+table<-table(chrom7_data$Topology_loose)
+AB_count<-table["AB_topology"]
+AC_count<-table["AC_topology"]
+BC_count<-table["BC_topology"]
+
+#Calculate the Dstat
+Dstat_observed<-(AC_count - AB_count)/(AC_count + AB_count)
+Dstat_observed<-unname(Dstat_observed)
+
+#Create function for bootstrapping
+Boot_Dstat <- function(X=chrom7_data$Topology_loose){
+  x.boot<-sample(X, size=length(X), replace=TRUE)
+  table<-table(x.boot)
+  AB_count<-table["AB_topology"]
+  AC_count<-table["AC_topology"]
+  Dstat_temp<-(AC_count - AB_count)/(AC_count + AB_count)
+  Dstat_temp<-unname(Dstat_temp)
+  Dstat_temp
+}
+#Perform bootstrap replication
+N<- 10000 #This indicates how many replicates to use
+boot.replicate <- replicate(N, Boot_Dstat())
+#Calculate P-value
+SD<-sd(boot.replicate)
+z<-(Dstat_observed-0)/SD
+P_value<-2*pnorm(-abs(z))
+out_chrom7<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
+
+#CHROM 8#
+chrom8_data<-subset(data_noOthers70BS, Scaffold == 8)
+
+table<-table(chrom8_data$Topology_loose)
+AB_count<-table["AB_topology"]
+AC_count<-table["AC_topology"]
+BC_count<-table["BC_topology"]
+
+#Calculate the Dstat
+Dstat_observed<-(AC_count - AB_count)/(AC_count + AB_count)
+Dstat_observed<-unname(Dstat_observed)
+
+#Create function for bootstrapping
+Boot_Dstat <- function(X=chrom8_data$Topology_loose){
+  x.boot<-sample(X, size=length(X), replace=TRUE)
+  table<-table(x.boot)
+  AB_count<-table["AB_topology"]
+  AC_count<-table["AC_topology"]
+  Dstat_temp<-(AC_count - AB_count)/(AC_count + AB_count)
+  Dstat_temp<-unname(Dstat_temp)
+  Dstat_temp
+}
+#Perform bootstrap replication
+N<- 10000 #This indicates how many replicates to use
+boot.replicate <- replicate(N, Boot_Dstat())
+#Calculate P-value
+SD<-sd(boot.replicate)
+z<-(Dstat_observed-0)/SD
+P_value<-2*pnorm(-abs(z))
+out_chrom8<-c(AC_count, AB_count, Dstat_observed, SD, z, P_value)
+
+#Concat the results from all the chromosomes
+cat_out_df_70BS<-c(out_full, out_chrom1, out_chrom2, out_chrom3, out_chrom4, out_chrom5, out_chrom6, out_chrom7, out_chrom8)
+
+#convert into dataframe
+cat_out_df_70BS<- data.frame(matrix(unlist(cat_out), nrow=9, byrow=TRUE))
+
+#append column names and rownames
+names(cat_out_df_70BS) <- c("AC_trees", "AB_trees", "Dstat_obs", "SD", "Z_score", "P_value")
+rownames(cat_out_df_70BS) <- c("Full_genome", "Chrom1", "Chrom2", "Chrom3", "Chrom4", "Chrom5", "Chrom6", "Chrom7", "Chrom8")
+
+cat_out_df_70BS
+
+################################################################
+##########Making tsv files for chrom mapping in coge############
+################################################################
+
+data1<-subset(data, Crub_seq != "No_Crub_tips")
+
+data_BC<-subset(data1, Topology_loose == "BC_topology")
+data_AC<-subset(data1, Topology_loose == "AC_topology")
+data_AB<-subset(data1, Topology_loose == "AB_topology")
+data_Othertop<-subset(data1, Topology_loose == "Other_topology")
+
+write.table(data_BC, file='data_BC.tsv', quote=FALSE, sep='\t', row.names=FALSE)
+write.table(data_AC, file='data_AC.tsv', quote=FALSE, sep='\t', row.names=FALSE)
+write.table(data_AB, file='data_AB.tsv', quote=FALSE, sep='\t', row.names=FALSE)
+write.table(data_Othertop, file='data_Othertop.tsv', quote=FALSE, sep='\t', row.names=FALSE)
+
+write.table(data1, file='ALL_genes.tsv', quote=FALSE, sep='\t', row.names=FALSE)
 
